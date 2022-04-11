3374e9b3d26353e7f5bd108f53eb8e986ea1d700
vikask08 <zvikas.8294@gmail.com>
2022-04-11 11:44:24 +0530

    commit2

---
+++ src/test/java/string/ReplaceSpacesWithChar.java
@@ -0,0 +1,13 @@
+package string;
+
+public class ReplaceSpacesWithChar {
+
+	public static void main(String[] args) {
+		String s1="Vikas Kumar ";
+		char ch1='s';
+		String re=s1.replace(' ',ch1 );
+		System.out.println(re);
+
+	}
+
+}
